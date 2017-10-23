ac720e0ae3b14613bafb09ad1df2c914a59ac841
EricA <eggsaladsandwich@Es-Mac.local>
2017-10-23 16:25:41 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var nameInput
+    $('.input').on( 'click', function(event) {
+        console.log(event, "event")
+     })
+        
+    
+    })
+
+    
+
+    // focus event , blur event, .target
+
+    //`<div><input type="text" name="name" value="${nameInput}"></div>`

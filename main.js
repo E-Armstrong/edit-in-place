df2cb070f1fcc7d5bcd1c5f0dd1e1dc2cb1cec39
EricA <eggsaladsandwich@Es-Mac.local>
2017-10-24 14:19:05 -0600

    Created basic format. Will toggle text but won't focus, or keep same text in paragraph/input

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
     var nameInput
-    $('.input').on( 'click', function(event) {
-        console.log(event, "event")
-     })
-        
-    
+    $('.user-name.input').on( 'click', function(event) {
+        nameInput = $('.user-name.input').text();
+        $('.name-input-div').toggleClass('hidden');
+        $('#name-input').focus();
+        })
+    $('#name-input').on( 'blur', function() {
+        console.log("blur is working")
+        $('.user-name.input').text(nameInput);
+        $('.name-input-div').toggleClass('hidden');
+        nameInput = $('.user-name.input').text();
     })
+    
+    
+})
 
     
 

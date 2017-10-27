e201407c9ad9de7c34d4632683ad929d2ee8bdae
EricA <eggsaladsandwich@Es-Mac.local>
2017-10-26 23:40:56 -0600

    Edit in place feature works for User Name! Needs to be edited to work for the Tell Us About Yourself section

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     var nameInput
-    $('.user-name.input').on( 'click', function(event) {
-        nameInput = $('.user-name.input').text();
-        $('.name-input-div').toggleClass('hidden');
+    $('#user-print').on( 'click', function(event) {
+        $('#name-input').toggleClass('hidden');
+        $('#user-print').toggleClass('hidden');
         $('#name-input').focus();
-        })
-    $('#name-input').on( 'blur', function() {
-        console.log("blur is working")
-        $('.user-name.input').text(nameInput);
-        $('.name-input-div').toggleClass('hidden');
-        nameInput = $('.user-name.input').text();
+        $('#name-input').val($('#user-print')[0].innerText)
     })
-    
-    
+    $('#name-input').on( 'blur', function() {
+        $('#name-input').toggleClass('hidden');
+        $('#user-print').toggleClass('hidden');
+        $('#user-print').text($('#name-input')[0].value)
+        console.log($('#name-input')[0].value)
+    })
 })
 
     
@@ -20,3 +19,6 @@ $(document).ready(function(){
     // focus event , blur event, .target
 
     //`<div><input type="text" name="name" value="${nameInput}"></div>`
+
+    var theInput = $(event.target).siblings()[0]
+    $(theInput).val($(event.target).text())
